afc39d1719e04d586a83f6efeaf49df03cba9ac4
stas <ru.usa.goo@gmail.com>
2024-07-04 20:28:04 +0300

    ~

---
+++ BlazorServerLib/TreeItemDataModel.cs
@@ -0,0 +1,15 @@
+﻿using MudBlazor;
+using SharedLib;
+
+namespace BlazorWebLib;
+
+/// <inheritdoc/>
+public class TreeItemDataModel : TreeItemData<EntryTagModel>
+{
+    /// <inheritdoc/>
+    public TreeItemDataModel(EntryTagModel entry, string icon) : base(entry)
+    {
+        Text = entry.Name;
+        Icon = icon;
+    }
+}
