4747d666dba3c93a559368cd8c7da72379a36e8d
stas <ru.usa.goo@gmail.com>
2024-07-26 09:33:16 +0300

    ~

---
+++ BlazorServerLib/Components/JournalUniversalComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @fakegov 
+////////////////////////////////////////////////
+
+using BlazorLib;
+
+namespace BlazorWebLib.Components;
+
+/// <summary>
+/// Журнал документов (универсальный)
+/// </summary>
+public partial class JournalUniversalComponent : BlazorBusyComponentBaseModel
+{
+
+}
