342bb8129c10598248bb13c2cb34bd90a5d24a52
stas <ru.usa.goo@gmail.com>
2024-07-28 08:01:25 +0300

    ~

---
+++ BlazorServerLib/Components/DocumentEditComponent.razor.cs
@@ -0,0 +1,30 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @fakegov 
+////////////////////////////////////////////////
+
+using BlazorLib;
+using Microsoft.AspNetCore.Components;
+using SharedLib;
+
+namespace BlazorWebLib.Components;
+
+public partial class DocumentEditComponent : BlazorBusyComponentBaseModel
+{
+    [Inject]
+    IJournalUniversalService JournalRepo { get; set; } = default!;
+
+
+    /// <summary>
+    /// Тип документа
+    /// </summary>
+    [Parameter, EditorRequired]
+    public required string DocumentNameOrId { get; set; }
+
+    /// <summary>
+    /// ProjectId
+    /// </summary>
+    [Parameter, EditorRequired]
+    public required int DocumentId { get; set; }
+
+
+}
