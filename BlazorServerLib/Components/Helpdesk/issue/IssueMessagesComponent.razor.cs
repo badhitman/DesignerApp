6f638edf8234db9837170a595ab474939d87b0ce
stas <ru.usa.goo@gmail.com>
2024-08-21 11:45:46 +0300

    ~

--- BlazorServerLib/Components/Helpdesk/issue/IssueMessagesComponent.razor.cs
+++ BlazorServerLib/Components/Helpdesk/issue/IssueMessagesComponent.razor.cs
@@ -4,7 +4,10 @@
 
 using BlazorLib;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using SharedLib;
+using System.Net.Http;
+using static MudBlazor.CategoryTypes;
 
 namespace BlazorWebLib.Components.Helpdesk.issue;
 
@@ -16,11 +19,42 @@ public partial class IssueMessagesComponent : IssueWrapBaseModel
     [Inject]
     IWebRemoteTransmissionService WebRemoteRepo { get; set; } = default!;
 
+    private IEnumerable<IssueMessageHelpdeskModelDB> Elements = [];
+
+    MudTable<IssueMessageHelpdeskModelDB> tableRef = default!;
 
     /// <summary>
     /// Добавляется новое сообщение
     /// </summary>
     public bool AddingNewMessage = false;
+
+    private string _searchStringQuery = "";
+    private string searchStringQuery
+    {
+        get => _searchStringQuery;
+        set
+        {
+            _searchStringQuery = value;
+            InvokeAsync(tableRef.ReloadServerData);
+        }
+    }
+
+    IssueMessageHelpdeskModelDB[]? messages;
+
+    /// <summary>
+    /// Here we simulate getting the paged, filtered and ordered data from the server
+    /// </summary>
+    private Task<TableData<IssueMessageHelpdeskModelDB>> ServerReload(TableState state, CancellationToken token)
+    {
+        if (messages is null)
+            return Task.FromResult(new TableData<IssueMessageHelpdeskModelDB>() { TotalItems = 0, Items = [] });
+
+        IssueMessageHelpdeskModelDB[] _messages = string.IsNullOrWhiteSpace(searchStringQuery)
+            ? messages
+            : [.. messages.Where(x => x.MessageText.Contains(searchStringQuery, StringComparison.OrdinalIgnoreCase))];
+
+        return Task.FromResult(new TableData<IssueMessageHelpdeskModelDB>() { TotalItems = _messages.Length, Items = _messages.Skip(state.PageSize * state.Page).Take(state.PageSize) });
+    }
 
     /// <summary>
     /// ReloadMessages
@@ -33,8 +67,9 @@ public partial class IssueMessagesComponent : IssueWrapBaseModel
             Payload = Issue.Id,
             SenderActionUserId = CurrentUser.UserId,
         });
-        SnackbarRepo.ShowMessagesResponse(messages_rest.Messages);
         IsBusyProgress = false;
+        messages = messages_rest.Response;
+        SnackbarRepo.ShowMessagesResponse(messages_rest.Messages);
         if (!messages_rest.Success() || messages_rest.Response is null)
             return;
 
@@ -55,5 +90,11 @@ public partial class IssueMessagesComponent : IssueWrapBaseModel
         }
 
         StateHasChanged();
+    }
+
+/// <inheritdoc/>
+    protected override async Task OnInitializedAsync()
+    {
+        await ReloadMessages();
     }
 }
