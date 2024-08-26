703e9d14cf2451663e857fe990376bc823acbea8
stas <ru.usa.goo@gmail.com>
2024-08-26 13:45:16 +0300

    ~

---
+++ BlazorServerLib/Components/Telegram/ChatsTableComponent.razor.cs
@@ -0,0 +1,33 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+using BlazorLib;
+using MudBlazor;
+using SharedLib;
+
+namespace BlazorWebLib.Components.Telegram;
+
+public partial class ChatsTableComponent : BlazorBusyComponentBaseModel
+{
+    private IEnumerable<ChatTelegramModelDB> pagedData = [];
+    private MudTable<ChatTelegramModelDB> table = default!;
+
+    private int totalItems;
+    private string? searchString = null;
+
+    /// <summary>
+    /// Here we simulate getting the paged, filtered and ordered data from the server
+    /// </summary>
+    private async Task<TableData<ChatTelegramModelDB>> ServerReload(TableState state, CancellationToken token)
+    {
+        
+        return new TableData<ChatTelegramModelDB>() { TotalItems = totalItems, Items = pagedData };
+    }
+
+    private void OnSearch(string text)
+    {
+        searchString = text;
+        table.ReloadServerData();
+    }
+}
