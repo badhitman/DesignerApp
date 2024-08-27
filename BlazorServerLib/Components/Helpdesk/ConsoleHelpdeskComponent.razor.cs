a4367a513c617b5541977e3c47167b964e2471e0
stas <ru.usa.goo@gmail.com>
2024-08-27 21:27:24 +0300

    ~

--- BlazorServerLib/Components/Helpdesk/ConsoleHelpdeskComponent.razor.cs
+++ BlazorServerLib/Components/Helpdesk/ConsoleHelpdeskComponent.razor.cs
@@ -2,8 +2,10 @@
 // © https://github.com/badhitman - @FakeGov 
 ////////////////////////////////////////////////
 
+using Microsoft.AspNetCore.Components;
 using BlazorLib;
 using SharedLib;
+using MudBlazor;
 
 namespace BlazorWebLib.Components.Helpdesk;
 
@@ -12,6 +14,36 @@ namespace BlazorWebLib.Components.Helpdesk;
 /// </summary>
 public partial class ConsoleHelpdeskComponent : BlazorBusyComponentBaseModel
 {
+    [Inject]
+    ISerializeStorageRemoteTransmissionService storageRepo { get; set; } = default!;
+
+    [Inject]
+    ISnackbar SnackbarRepo { get; set; } = default!;
+
+
     readonly List<HelpdeskIssueStepsEnum> Steps = [.. Enum.GetValues(typeof(HelpdeskIssueStepsEnum)).Cast<HelpdeskIssueStepsEnum>()];
     byte stepNum;
+    bool IsLarge;
+
+    static StorageCloudParameterModel KeyStorage => new()
+    {
+        ApplicationName = "",
+        Name = "",
+        PrefixPropertyName = ""
+    };
+
+    async Task ToggleSize()
+    {
+        IsLarge = !IsLarge;
+    }
+
+    /// <inheritdoc/>
+    protected override async Task OnInitializedAsync()
+    {
+        IsBusyProgress = true;
+        TResponseModel<bool> res = await storageRepo.ReadParameter<bool>(KeyStorage);
+        IsBusyProgress = false;
+        SnackbarRepo.ShowMessagesResponse(res.Messages);
+        IsLarge = res.Response == true;
+    }
 }
