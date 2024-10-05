7ecedf1f0c359f9dca52ad937e8cab37131fc063
stas <ru.usa.goo@gmail.com>
2024-10-05 07:28:46 +0300

    ~

---
+++ DBContextLibs/DbPostgreLib/Migrations/Helpdesk/20241005043009_HelpdeskPostgreContext002.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbPostgreLib.Migrations.Helpdesk
+{
+    /// <inheritdoc />
+    public partial class HelpdeskPostgreContext002 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_AuthorIdentityUserId_ExecutorIdentityUserId_LastUpda~",
+                table: "Issues");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_AuthorIdentityUserId_ExecutorIdentityUserId_LastUpda~",
+                table: "Issues",
+                columns: new[] { "AuthorIdentityUserId", "ExecutorIdentityUserId", "LastUpdateAt", "RubricIssueId", "StepIssue" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_CreatedAt",
+                table: "Issues",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_LastUpdateAt",
+                table: "Issues",
+                column: "LastUpdateAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_AuthorIdentityUserId_ExecutorIdentityUserId_LastUpda~",
+                table: "Issues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_CreatedAt",
+                table: "Issues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Issues_LastUpdateAt",
+                table: "Issues");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_AuthorIdentityUserId_ExecutorIdentityUserId_LastUpda~",
+                table: "Issues",
+                columns: new[] { "AuthorIdentityUserId", "ExecutorIdentityUserId", "LastUpdateAt" });
+        }
+    }
+}
--- DBContextLibs/DbPostgreLib/migrations.md
+++ DBContextLibs/DbPostgreLib/migrations.md
@@ -1,20 +1,20 @@
 ```
-Add-Migration MainPostgreContext001 -Context MainDbAppContext -Project DbPostgreLib -StartupProject BlankBlazorApp
+Add-Migration MainPostgreContext002 -Context MainDbAppContext -Project DbPostgreLib -StartupProject BlankBlazorApp
 Update-Database -Context MainDbAppContext -Project DbPostgreLib -StartupProject BlankBlazorApp
 ```
 
 ```
-Add-Migration StorageContext001 -Context StorageContext -Project DbPostgreLib -StartupProject StorageService
+Add-Migration StorageContext002 -Context StorageContext -Project DbPostgreLib -StartupProject StorageService
 Update-Database -Context StorageContext -Project DbPostgreLib -StartupProject StorageService
 ```
 
 ```
-Add-Migration HelpdeskPostgreContext001 -Context HelpdeskContext -Project DbPostgreLib -StartupProject HelpdeskService
+Add-Migration HelpdeskPostgreContext003 -Context HelpdeskContext -Project DbPostgreLib -StartupProject HelpdeskService
 Update-Database -Context HelpdeskContext -Project DbPostgreLib -StartupProject HelpdeskService
 ```
 
 ```
-Add-Migration TelegramBotContext001 -Context TelegramBotContext -Project DbPostgreLib -StartupProject Telegram.Bot.Polling
+Add-Migration TelegramBotContext002 -Context TelegramBotContext -Project DbPostgreLib -StartupProject Telegram.Bot.Polling
 Update-Database -Context TelegramBotContext -Project DbPostgreLib -StartupProject Telegram.Bot.Polling
 ```
 
