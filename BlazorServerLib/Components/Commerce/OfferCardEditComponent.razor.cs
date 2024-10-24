c644da266f456e276c00ba08f99b3a61de089aed
stas <ru.usa.goo@gmail.com>
2024-10-24 11:26:34 +0300

    ~

--- BlazorServerLib/Components/Commerce/OfferCardEditComponent.razor.cs
+++ BlazorServerLib/Components/Commerce/OfferCardEditComponent.razor.cs
@@ -27,6 +27,9 @@ public partial class OfferCardEditComponent : BlazorBusyComponentBaseAuthModel
 
     OfferGoodModelDB CurrentOffer = default!;
     OfferGoodModelDB editOffer = default!;
+    FilesContextViewComponent? filesViewRef;
+    string images_upload_url = default!;
+    Dictionary<string, object> editorConf = default!;
 
     bool CanSave =>
         editOffer.IsDisabled != CurrentOffer.IsDisabled ||
@@ -44,11 +47,17 @@ public partial class OfferCardEditComponent : BlazorBusyComponentBaseAuthModel
         SnackbarRepo.ShowMessagesResponse(res.Messages);
         if (res.Success() && res.Response > 0)
             CurrentOffer = GlobalTools.CreateDeepCopy(editOffer)!;
+
+        if (filesViewRef is not null)
+            await filesViewRef.ReloadServerData();
     }
 
     /// <inheritdoc/>
     protected override async Task OnInitializedAsync()
     {
+        images_upload_url = $"{GlobalStaticConstants.TinyMCEditorUploadImage}{GlobalStaticConstants.Routes.OFFER_CONTROLLER_NAME}/{GlobalStaticConstants.Routes.BODY_CONTROLLER_NAME}?{nameof(StorageMetadataModel.PrefixPropertyName)}={GlobalStaticConstants.Routes.IMAGE_ACTION_NAME}&{nameof(StorageMetadataModel.OwnerPrimaryKey)}={OfferId}";
+        editorConf = GlobalStaticConstants.TinyMCEditorConf(images_upload_url);
+
         await SetBusy();
         await ReadCurrentUser();
         TResponseModel<OfferGoodModelDB[]> res = await CommerceRepo.OffersRead([OfferId]);
