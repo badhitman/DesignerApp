c013dfc8204d55abdab31cccf0ad5988182f1c8d
stas <ru.usa.goo@gmail.com>
2024-10-28 05:04:44 +0300

    ~

---
+++ DBContextLibs/DbPostgreLib/Migrations/Storage/20241028020415_StorageContext014.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbPostgreLib.Migrations.Storage
+{
+    /// <inheritdoc />
+    public partial class StorageContext014 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TokenAccess",
+                table: "CloudFiles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TokenAccess",
+                table: "CloudFiles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
