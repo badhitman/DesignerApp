398c038bbd2e503a58b465f51547dba18651deee
stas <ru.usa.goo@gmail.com>
2024-11-04 15:21:13 +0300

    ~

---
+++ ToolsMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ToolsMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ ToolsMauiApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToolsMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
