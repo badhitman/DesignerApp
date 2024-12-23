769e88d4ed6a94976f4aa797079a41ded205e5a6
stas <ru.usa.goo@gmail.com>
2024-12-23 10:37:08 +0300

    ~

---
+++ DBContextLibs/DbPostgreLib/Migrations/Commerce/20241223073553_CommerceContext034.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbPostgreLib.Migrations.Commerce
+{
+    /// <inheritdoc />
+    public partial class CommerceContext034 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ContractorsOrganizations_OrganizationId",
+                table: "ContractorsOrganizations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContractorsOrganizations_OrganizationId_OfferId",
+                table: "ContractorsOrganizations",
+                columns: new[] { "OrganizationId", "OfferId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ContractorsOrganizations_OrganizationId_OfferId",
+                table: "ContractorsOrganizations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContractorsOrganizations_OrganizationId",
+                table: "ContractorsOrganizations",
+                column: "OrganizationId");
+        }
+    }
+}
