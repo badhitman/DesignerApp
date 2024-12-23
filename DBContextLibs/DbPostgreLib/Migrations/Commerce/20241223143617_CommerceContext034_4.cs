a46790b736c51831fa319693f5e1a0633e9395dd
stas <ru.usa.goo@gmail.com>
2024-12-23 17:40:23 +0300

    ~

---
+++ DBContextLibs/DbPostgreLib/Migrations/Commerce/20241223143617_CommerceContext034_4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbPostgreLib.Migrations.Commerce
+{
+    /// <inheritdoc />
+    public partial class CommerceContext034_4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndPart",
+                table: "OrdersAttendances");
+
+            migrationBuilder.DropColumn(
+                name: "StartPart",
+                table: "OrdersAttendances");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EndPart",
+                table: "OrdersAttendances",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "StartPart",
+                table: "OrdersAttendances",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+    }
+}
