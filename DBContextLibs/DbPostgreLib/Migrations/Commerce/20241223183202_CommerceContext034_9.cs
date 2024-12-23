6181f425440b3b9a2aede9117ccdde936e265b3d
stas <ru.usa.goo@gmail.com>
2024-12-23 21:32:14 +0300

    ~

---
+++ DBContextLibs/DbPostgreLib/Migrations/Commerce/20241223183202_CommerceContext034_9.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbPostgreLib.Migrations.Commerce
+{
+    /// <inheritdoc />
+    public partial class CommerceContext034_9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
