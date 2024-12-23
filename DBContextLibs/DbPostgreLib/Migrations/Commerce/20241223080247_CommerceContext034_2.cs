d3c0abf3efebc5e817859bfcb91bb08f276dd6b7
stas <ru.usa.goo@gmail.com>
2024-12-23 11:04:03 +0300

    ~

---
+++ DBContextLibs/DbPostgreLib/Migrations/Commerce/20241223080247_CommerceContext034_2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbPostgreLib.Migrations.Commerce
+{
+    /// <inheritdoc />
+    public partial class CommerceContext034_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LockerOffersAvailability",
+                table: "LockerOffersAvailability");
+
+            migrationBuilder.RenameTable(
+                name: "LockerOffersAvailability",
+                newName: "LockersTransactions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LockerOffersAvailability_LockerId_LockerName_RubricId",
+                table: "LockersTransactions",
+                newName: "IX_LockersTransactions_LockerId_LockerName_RubricId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LockersTransactions",
+                table: "LockersTransactions",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LockersTransactions",
+                table: "LockersTransactions");
+
+            migrationBuilder.RenameTable(
+                name: "LockersTransactions",
+                newName: "LockerOffersAvailability");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_LockersTransactions_LockerId_LockerName_RubricId",
+                table: "LockerOffersAvailability",
+                newName: "IX_LockerOffersAvailability_LockerId_LockerName_RubricId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LockerOffersAvailability",
+                table: "LockerOffersAvailability",
+                column: "Id");
+        }
+    }
+}
