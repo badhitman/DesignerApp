ecf92b0d8f310265e5b4998d415b605869946323
stas <ru.usa.goo@gmail.com>
2025-01-15 18:12:20 +0300

    ~

--- devops/prod-builds.update.sh
+++ devops/prod-builds.update.sh
@@ -1,4 +1,4 @@
-systemctl stop web.app.service comm.app.service tg.app.service api.app.service bus.app.service constructor.app.service hd.app.service
+systemctl stop web.app.service comm.app.service tg.app.service api.app.service bus.app.service constructor.app.service hd.app.service identity.app.service
 7z a /srv/Cloud.Disk/services-snapshots/all_services_`date +%Y-%m-%d"_"%H_%M_%S`.7z /srv/services/*
 rm -r /srv/services/*
 cp -r /srv/git/builds/ApiRestService /srv/services/ApiRestService
@@ -8,6 +8,7 @@ cp -r /srv/git/builds/HelpdeskService /srv/services/HelpdeskService
 cp -r /srv/git/builds/ConstructorService /srv/services/ConstructorService
 cp -r /srv/git/builds/Telegram.Bot.Polling /srv/services/Telegram.Bot.Polling
 cp -r /srv/git/builds/BlankBlazorApp /srv/services/BlankBlazorApp
+cp -r /srv/git/builds/IdentityService /srv/services/IdentityService
 chown -R www-data:www-data /srv/services
 chmod -R 777 /srv/services
-systemctl start comm.app.service web.app.service bus.app.service tg.app.service api.app.service hd.app.service constructor.app.service
+systemctl start comm.app.service web.app.service bus.app.service tg.app.service api.app.service hd.app.service constructor.app.service identity.app.service
