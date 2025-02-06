2ec53c17c0db8650f14b32d935de2e11a484fdf0
stas <ru.usa.goo@gmail.com>
2025-02-06 07:03:58 +0300

    ~

--- ApiRestService/RolesAuthorizationFilter.cs
+++ ApiRestService/RolesAuthorizationFilter.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Mvc;
 using SharedLib;
-using Microsoft.Extensions.Options;
 
 namespace ApiRestService;
 
@@ -12,8 +12,8 @@ namespace ApiRestService;
 [AttributeUsage(AttributeTargets.All)]
 public class RolesAuthorizationFilter(IOptions<RestApiConfigBaseModel> _conf, string? Roles = null) : Attribute, IAuthorizationFilter
 {
-    HttpContext _http_context = default!;
     static ReadOnlySpan<char> Separator => [' ', ',', '\t', '\n', '\r'];
+    HttpContext _http_context = default!;
 
     /// <inheritdoc/>
     public void OnAuthorization(AuthorizationFilterContext context)
@@ -32,7 +32,7 @@ public class RolesAuthorizationFilter(IOptions<RestApiConfigBaseModel> _conf, st
         context.HttpContext.Response.Headers.Append(nameof(perm.User), user_name);
 
         string[] roles = Roles?.Split(Separator).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray() ?? [];
-        if (roles.Length != 0 && perm.Roles?.Any(x => roles.Any(y => y.Equals(x.ToString(), StringComparison.OrdinalIgnoreCase) || y.Equals(x.DescriptionInfo(), StringComparison.OrdinalIgnoreCase))) != true)
+        if (roles.Length != 0 && perm.Roles?.Any(x => roles.Any(y => y.Equals(x.ToString(), StringComparison.OrdinalIgnoreCase))) != true)
             context.Result = new ForbidResult();
     }
 
@@ -44,6 +44,7 @@ public class RolesAuthorizationFilter(IOptions<RestApiConfigBaseModel> _conf, st
 
         if (_http_context.Request.Headers.TryGetValue(_conf.Value!.TokenAccessHeaderName, out StringValues tok))
             return tok.FirstOrDefault() ?? string.Empty;
+
         if (_http_context.Request.Query.Any(x => x.Key.Equals(_conf.Value!.TokenAccessHeaderName)))
             return _http_context.Request.Query[_conf.Value!.TokenAccessHeaderName]!;
 
